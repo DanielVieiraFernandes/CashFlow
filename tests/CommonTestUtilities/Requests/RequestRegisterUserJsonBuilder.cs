43ac5ade6aa4b7199074aba090dfc87d4e909b58
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-11-09 17:31:23 -0300

    Adiciona validações e testes para registro de usuários
    Inclui a classe `RequestRegisterUserJsonBuilder` para gerar
    dados fictícios de requisições de registro de usuários usando
    a biblioteca `Bogus`.
    
    Adiciona a classe de teste `PasswordValidatorTest` para validar
    senhas inválidas, cobrindo cenários como senhas vazias, curtas
    ou sem complexidade.
    
    Adiciona a classe de teste `RegisterUserValidatorTest` para
    validar requisições de registro de usuários. Testa casos de
    sucesso e falhas, incluindo validações para campos `Name`,
    `Email` e `Password`, com mensagens de erro apropriadas.

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterUserJsonBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestRegisterUserJsonBuilder
+{
+    public static RequestRegisterUserJson Build()
+    {
+        return new Faker<RequestRegisterUserJson>()
+            .RuleFor(f => f.Name, faker => faker.Person.FirstName)
+            .RuleFor(f => f.Email, (faker, user) => faker.Internet.Email(user.Name))
+            .RuleFor(f => f.Password, faker => faker.Internet.Password(prefix: "!Aa1"));
+    }
+}
---
+++ tests/Validators.Tests/Users/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,97 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Tests.Users.Register;
+public class RegisterUserValidatorTest
+{
+
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var request = RequestRegisterUserJsonBuilder.Build();
+        var validator = new RegisterUserValidator();
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("          ")]
+    [InlineData(null)]
+    public void Error_Name_Empty(string name)
+    {
+        //Arrange
+        var request = RequestRegisterUserJsonBuilder.Build();
+        var validator = new RegisterUserValidator();
+
+        request.Name = name;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage == ResourceErrorMessages.NAME_EMPTY);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("          ")]
+    [InlineData(null)]
+    public void Error_Email_Empty(string email)
+    {
+        //Arrange
+        var request = RequestRegisterUserJsonBuilder.Build();
+        var validator = new RegisterUserValidator();
+
+        request.Email = email;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage == ResourceErrorMessages.EMAIL_EMPTY);
+    }
+
+    [Fact]
+    public void Error_Email_Invalid()
+    {
+        //Arrange
+        var request = RequestRegisterUserJsonBuilder.Build();
+        var validator = new RegisterUserValidator();
+
+        request.Email = "daniel.com";
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage == ResourceErrorMessages.EMAIL_INVALID);
+    }
+
+    [Fact]
+    public void Error_Password_Empty()
+    {
+        //Arrange
+        var request = RequestRegisterUserJsonBuilder.Build();
+        var validator = new RegisterUserValidator();
+
+        request.Password = string.Empty;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage == ResourceErrorMessages.INVALID_PASSWORD);
+    }
+}
