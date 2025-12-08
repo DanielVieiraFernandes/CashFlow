1c277cbec48b4fe5c97e1ec47ae9a60d9ffea355
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-12-08 20:29:57 -0300

    test: adição de builders de teste para Expense e repositório
    Foram incluídos os arquivos ExpenseBuilder.cs e ExpensesReadOnlyRepositoryBuilder.cs para facilitar a criação de entidades Expense e a simulação do repositório IExpensesReadOnlyRepository em testes unitários. O pacote Bogus foi ajustado para a versão 35.5.1 no projeto CommonTestUtilities.csproj. Essas mudanças melhoram a organização e a eficiência dos testes automatizados.

---
+++ tests/CommonTestUtilities/Repositories/ExpensesReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,40 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories.Expenses;
+using Moq;
+
+namespace CommonTestUtilities.Repositories;
+
+public class ExpensesReadOnlyRepositoryBuilder
+{
+    private readonly Mock<IExpensesReadOnlyRepository> _repository;
+
+    public ExpensesReadOnlyRepositoryBuilder()
+    {
+        _repository = new Mock<IExpensesReadOnlyRepository>();
+    }
+
+    public ExpensesReadOnlyRepositoryBuilder GetAll(User user, List<Expense> expenses)
+    {
+        //***************************************************************************
+        // Define que ao chamar o método GetAll com o usuário especificado,
+        // o repositório irá retornar a lista de despesas fornecida para o mock.
+        //***************************************************************************
+        _repository.Setup(repository => repository.GetAll(user)).ReturnsAsync(expenses);
+
+        return this;
+    }
+
+    public ExpensesReadOnlyRepositoryBuilder GetById(User user, Expense? expense)
+    {
+        //***************************************************************************************
+        // Define que ao chamar o método GetById com o usuário e o ID da despesa especificados,
+        // o repositório irá retornar a despesa fornecida para o mock.
+        //***************************************************************************************
+        if (expense is not null)
+            _repository.Setup(repository => repository.GetById(user, expense.Id)).ReturnsAsync(expense);
+
+        return this;
+    }
+
+    public IExpensesReadOnlyRepository Build() => _repository.Object;
+}
