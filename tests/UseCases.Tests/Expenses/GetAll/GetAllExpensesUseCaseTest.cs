d9a785bb8b99dddd3a9b31e6fbb609a661c1b179
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-12-08 20:30:20 -0300

    test: adiciona testes unitários para casos de uso de despesas
    Foram criados dois testes unitários: GetAllExpensesUseCaseTest e GetExpenseByIdUseCaseTest. Os testes validam os cenários de recuperação de todas as despesas e de uma despesa por ID, incluindo casos de sucesso e erro (despesa não encontrada). Utilizam builders e repositórios mockados para simular dados e contexto do usuário, garantindo a integridade das regras de negócio e tratamento adequado de exceções.

---
+++ tests/UseCases.Tests/Expenses/GetAll/GetAllExpensesUseCaseTest.cs
@@ -0,0 +1,49 @@
+﻿using CashFlow.Application.UseCases.Expenses.GetAll;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using FluentAssertions;
+
+namespace UseCases.Tests.Expenses.GetAll;
+
+public class GetAllExpensesUseCaseTest
+{
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+    // Testes de Unidade do caso de uso de recuperar despesas
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+    [Fact]
+    public async Task Success()
+    {
+        var loggedUser = UserBuilder.Build();
+        var expenses = ExpenseBuilder.Collection(loggedUser);
+
+        var useCase = CreateUseCase(loggedUser, expenses);
+
+        var result = await useCase.Execute();
+
+        result.Should().NotBeNull();
+
+        //************************************************************
+        // Verifico se a lista não está vazia
+        // e se todos os elementos da lista satisfazem as regras 
+        //************************************************************
+        result.Expenses.Should().NotBeNullOrEmpty().And.AllSatisfy(expense =>
+        {
+            expense.Id.Should().BeGreaterThan(0);
+            expense.Title.Should().NotBeNullOrEmpty();
+            expense.Amount.Should().BeGreaterThan(0);
+        });
+    }
+
+    private GetAllExpensesUseCase CreateUseCase(User user, List<Expense> expenses)
+    {
+        var repository = new ExpensesReadOnlyRepositoryBuilder().GetAll(user, expenses).Build();
+        var mapper = MapperBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+
+        return new GetAllExpensesUseCase(repository, mapper, loggedUser);
+    }
+}
