e380490d1c31d30ec26960e22b20909ecb9a4402
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-12-11 20:25:23 -0300

    test: finalizando teste unitário do caso de uso que deleta uma despesa por id.

---
+++ tests/UseCases.Tests/Expenses/Delete/DeleteExpenseByIdUseCaseTest.cs
@@ -0,0 +1,60 @@
+﻿using CashFlow.Application.UseCases.Expenses.Delete;
+using CashFlow.Domain.Entities;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using FluentAssertions;
+
+namespace WebApi.Test.Expenses.Delete;
+
+public class DeleteExpenseByIdUseCaseTest
+{
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+    // Testes de unidade para o caso de uso DeleteExpenseUseCase
+    //
+    // Cenários: 
+    // - Sucesso 
+    // - Erro: Expense não encontrado
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+    [Fact]
+    public async Task Success()
+    {
+        var loggedUser = UserBuilder.Build();
+        var expense = ExpenseBuilder.Build(loggedUser);
+
+        var useCase = CreateUseCase(loggedUser, expense);
+
+        // Guardo a ação para verificação posterior
+        var act = async () => await useCase.Execute(expense.Id);
+
+        // Verifico se a ação não lança exceção
+        await act.Should().NotThrowAsync();
+    }
+
+    [Fact]
+    public async Task Error_Expense_Not_Found()
+    {
+        var loggedUser = UserBuilder.Build();
+
+        var useCase = CreateUseCase(loggedUser);
+
+        var act = async () => await useCase.Execute(9999);
+
+        var result = await act.Should().ThrowAsync<NotFoundException>();
+
+        result.Where(e => e.GetErrors().Count == 1 && e.GetErrors().Contains(ResourceErrorMessages.EXPENSE_NOT_FOUND));
+    }
+
+    private DeleteExpenseUseCase CreateUseCase(User user, Expense? expense = null)
+    {
+        var repositoryWriteOnly = ExpensesWriteOnlyRepositoryBuilder.Build();
+        var repository = new ExpensesReadOnlyRepositoryBuilder().GetById(user, expense).Build();
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+
+        return new DeleteExpenseUseCase(repository, repositoryWriteOnly, unitOfWork, loggedUser);
+    }
+}
