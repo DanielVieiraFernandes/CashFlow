9c3bcac156a35d9bc87bb82977e7ca3ee11cc1e2
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-12-24 21:44:51 -0300

    feat: adicionando tabela tag no banco

--- src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infraestructure/DataAccess/CashFlowDbContext.cs
@@ -1,15 +1,41 @@
-﻿using CashFlow.Domain.Entities;
+﻿//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// Comando que cria uma migration: 
+// 
+// dotnet ef migrations add TagsIntoExpenses --project.\CashFlow.Infraestructure\
+// --startup-project.\CashFlow.Api\
+//
+// O comando deve ser executado na pasta src do projeto.
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+
+using CashFlow.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infraestructure.DataAccess;
+
 public class CashFlowDbContext : DbContext
 {
-    public CashFlowDbContext(DbContextOptions options) : base(options) // estou repassando para o construtor
-                                                                       // da classe base as options
-                                                                       // que estou recebendo
+
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+    // estou repassando para o construtor
+    // da classe base as options
+    // que estou recebendo
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+    public CashFlowDbContext(DbContextOptions options) : base(options)
     {
 
     }
     public DbSet<Expense> Expenses { get; set; }
     public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //*************************************************
+        // Sobrescrevo o método para que a migration
+        // crie a tabela no plural
+        //*************************************************
+        modelBuilder.Entity<Tag>().ToTable("Tags");
+    }
 }
