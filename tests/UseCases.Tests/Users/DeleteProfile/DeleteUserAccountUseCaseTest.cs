c571fbdaa89246bdee665b1ab250dda60a650e7c
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-12-24 21:10:10 -0300

    test: adicionando testes de unidade e integração para o caso de uso que deleta um usuário.
    Foram adicionados dois novos arquivos de teste para cobrir a funcionalidade de exclusão de conta de usuário. O arquivo DeleteUserAccountUseCaseTest.cs implementa testes unitários para o caso de uso, garantindo que a exclusão ocorre sem exceções. O arquivo DeleteUserAccountTest.cs traz testes de integração para o endpoint de deleção, validando que o usuário não consegue mais acessar após a exclusão. Esses testes aumentam a cobertura e a confiabilidade do sistema.

---
+++ tests/UseCases.Tests/Users/DeleteProfile/DeleteUserAccountUseCaseTest.cs
@@ -0,0 +1,39 @@
+﻿using CashFlow.Application.UseCases.Users.DeleteProfile;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using FluentAssertions;
+
+namespace UseCases.Tests.Users.DeleteProfile;
+
+public class DeleteUserAccountUseCaseTest
+{
+
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+    // Testes de unidade para o caso de uso DeleteUserAccountUseCase
+    // 
+    // Cenários: 
+    // - Sucesso na deleção da conta do usuário
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+    [Fact]
+    public async Task Success()
+    {
+        var loggedUser = UserBuilder.Build();
+        var useCase = CreateUseCase(loggedUser);
+
+        var act = async () => await useCase.Execute();
+
+        await act.Should().NotThrowAsync();
+    }
+
+    private DeleteUserAccountProfileUseCase CreateUseCase(User user)
+    {
+        var repository = UserWriteOnlyRepositoryBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var unitOfWork = UnitOfWorkBuilder.Build();
+
+        return new DeleteUserAccountProfileUseCase(loggedUser, repository, unitOfWork);
+    }
+}
---
+++ tests/WebApi.Test/Users/DeleteProfile/DeleteUserAccountTest.cs
@@ -0,0 +1,37 @@
+﻿using CashFlow.Communication.Requests;
+using FluentAssertions;
+using System.Net;
+
+namespace WebApi.Test.Users.DeleteProfile;
+
+public class DeleteUserAccountTest : CashFlowClassFixture
+{
+    private const string METHOD = "api/user";
+    private readonly string _token;
+    private readonly string _email;
+    private readonly string _password;
+    public DeleteUserAccountTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+    {
+        _token = webApplicationFactory.User_Team_Member.GetToken();
+        _email = webApplicationFactory.User_Team_Member.GetEmail();
+        _password = webApplicationFactory.User_Team_Member.GetPassword();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var result = await DoDelete(requestUri: METHOD, token: _token);
+
+        result.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+        RequestLoginJson request = new()
+        {
+            Email = _email,
+            Password = _password
+        };
+
+        result = await DoPost(requestUri: $"api/login", request, token: _token);
+
+        result.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+}
